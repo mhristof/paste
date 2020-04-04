f724b890225600c0e92722bc41e3292ac146cdbd
Mike Christof <mhristof@gmail.com>
2020-04-04 17:02:14 +0100

    redact aws stuff

--- redact/main.go
+++ redact/main.go
@@ -1,9 +1,11 @@
 package redact
 
 import (
+	"fmt"
 	"io/ioutil"
 	"os"
 	"regexp"
+	"strings"
 )
 
 func Redact() string {
@@ -17,15 +19,48 @@ func Redact() string {
 
 func convert(lines []byte) string {
 	out := removeAWSCreds(string(lines))
+	out = removeUser(out)
+	out = removeInstanceID(out)
 	return out
 }
 
-func removeAWSCreds(lines string) string {
-	access := regexp.MustCompile(`(\W{0,})[A-Z0-9]{16}([A-Z0-9]{4})(\W|$)`)
-	res := access.ReplaceAllString(lines, "${1}AKIAIOSXFEXAMPLE${2}${3}")
+func removeInstanceID(lines string) string {
+	oldID := regexp.MustCompile(`i-\w{5}(\w{3})`)
+	res := oldID.ReplaceAllString(lines, "i-12345${1}")
 
-	secret := regexp.MustCompile(`([A-Za-z0-9/+=]{40})(\W|$)`)
-	res = secret.ReplaceAllString(res, "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY")
+	newID := regexp.MustCompile(`i-\w{14}(\w{3})`)
+	res = newID.ReplaceAllString(res, "i-1234567890abcd${1}")
 
 	return res
+}
+
+func removeUser(lines string) string {
+	user := os.Getenv("USER")
+	if user == "" {
+		return lines
+	}
+
+	return strings.ReplaceAll(
+		lines,
+		user,
+		"user",
+	)
+}
+
+func removeAWSCreds(lines string) string {
+	// instead of complicating the regex to take into consideration the tocken in
+	// the start of the line and in the end, pad it with a known prefix and
+	// remove it at the end
+	padded := fmt.Sprintf("-%s-", lines)
+
+	access := regexp.MustCompile(`(\W)[A-Z0-9]{16}([A-Z0-9]{4})(\W)`)
+	res := access.ReplaceAllString(padded, "${1}AKIAIOSXFEXAMPLE${2}${3}")
+
+	secret := regexp.MustCompile(`(\W)([A-Za-z0-9/+=]{35})([A-Za-z0-9/+=]{5})(\W)`)
+	res = secret.ReplaceAllString(res, "${1}wJalrMI/K7MDENG/bPxRfiCYEXAMPLEKEY/${3}${4}")
+
+	return strings.TrimSuffix(
+		strings.TrimPrefix(res, "-"),
+		"-",
+	)
 }
--- redact/main_test.go
+++ redact/main_test.go
@@ -1,6 +1,8 @@
 package redact
 
 import (
+	"fmt"
+	"os"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -26,6 +28,51 @@ func TestConvert(t *testing.T) {
 			name: "AWS key at the start",
 			in:   []byte("AKIA12345678C1234567 is an AWS key"),
 			out:  "AKIAIOSXFEXAMPLE4567 is an AWS key",
+		},
+		{
+			name: "Not an AWS key",
+			in:   []byte("AAKIA12345678C1234567 is not an AWS key"),
+			out:  "AAKIA12345678C1234567 is not an AWS key",
+		},
+		{
+			name: "AWS secret at the end",
+			in:   []byte("This is an AWS secret key wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY"),
+			out:  "This is an AWS secret key wJalrMI/K7MDENG/bPxRfiCYEXAMPLEKEY/LEKEY",
+		},
+		{
+			name: "AWS secret at the middle",
+			in:   []byte("This is an AWS secret key wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY."),
+			out:  "This is an AWS secret key wJalrMI/K7MDENG/bPxRfiCYEXAMPLEKEY/LEKEY.",
+		},
+		{
+			name: "AWS secret at the start",
+			in:   []byte("wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY is an AWS key"),
+			out:  "wJalrMI/K7MDENG/bPxRfiCYEXAMPLEKEY/LEKEY is an AWS key",
+		},
+		{
+			name: "not an AWS secret",
+			in:   []byte("EwJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY is not a key"),
+			out:  "EwJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY is not a key",
+		},
+		{
+			name: "Multiple keys",
+			in:   []byte("wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY and wJ11111tnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY are keys"),
+			out:  "wJalrMI/K7MDENG/bPxRfiCYEXAMPLEKEY/LEKEY and wJalrMI/K7MDENG/bPxRfiCYEXAMPLEKEY/LEKEY are keys",
+		},
+		{
+			name: "User name",
+			in:   []byte(fmt.Sprintf("This is the user %s", os.Getenv("USER"))),
+			out:  "This is the user user",
+		},
+		{
+			name: "Old instance id",
+			in:   []byte("This is an old instance id i-98765432"),
+			out:  "This is an old instance id i-12345432",
+		},
+		{
+			name: "New instance id",
+			in:   []byte("This is a new instance id i-5c7b139248ad47f56"),
+			out:  "This is a new instance id i-1234567890abcdf56",
 		},
 	}
 
