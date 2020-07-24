1352d9d3ed9a10d3c78f86b77713dd02a4da1ab0
Mike Christof <mhristof@gmail.com>
2020-07-24 15:21:01 +0100

    feature lower and upper case

---
+++ cmd/lower.go
@@ -0,0 +1,29 @@
+package cmd
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"strings"
+
+	"github.com/spf13/cobra"
+)
+
+var (
+	lowerCmd = &cobra.Command{
+		Use:   "lower",
+		Short: "Convert to lower case",
+		Run: func(cmd *cobra.Command, args []string) {
+			Verbose(cmd)
+			data, err := ioutil.ReadAll(os.Stdin)
+			if err != nil {
+				panic(err)
+			}
+			fmt.Println(strings.ToLower(string(data)))
+		},
+	}
+)
+
+func init() {
+	rootCmd.AddCommand(lowerCmd)
+}
